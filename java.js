7381b13e740fc7de7a11f0d8b28524c2fcb8326f
Trice3zz <Dariustrice.98@gmail.com>
2025-02-20 20:57:23 -0500

    ddd

--- java.js
+++ java.js
@@ -1,2 +1,9 @@
 alert("Hope Your Day Is Going Well,Today We Learn About Darius Trice!")
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
 
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
